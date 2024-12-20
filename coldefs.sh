59272d91dcdbf1d9147d65be871183b6304f9f1e
lulboi6000 <161829134+lulboi6000@users.noreply.github.com>
2024-12-20 14:09:47 +0600

    Add files via upload

---
+++ coldefs.sh
@@ -0,0 +1,49 @@
+#! /bin/bash
+
+# forground normal colors
+
+fblack="\033[1;30m"
+fred="\033[1;31m"
+fgreen="\033[1;32m"
+fyellow="\033[1;33m"
+fblue="\033[1;34m"
+fmagenta="\033[1;35m"
+fcyan="\033[1;36m"
+fwhite="\033[1;37m"
+
+# forground bright colors
+
+fbblack="\033[1;90m"
+fbred="\033[1;91m"
+fbgreen="\033[1;92m"
+fbyellow="\033[1;93m"
+fbblue="\033[1;94m"
+fbmagenta="\033[1;95m"
+fbcyan="\033[1;96m"
+fbwhite="\033[1;97m"
+
+# background normal colors
+
+bblack="\033[1;40m"
+bred="\033[1;41m"
+bgreen="\033[1;42m"
+byellow="\033[1;43m"
+bblue="\033[1;44m"
+bmagenta="\033[1;45m"
+bcyan="\033[1;46m"
+bwhite="\033[1;47m"
+
+# background bright colors
+
+bbblack="\033[1;100m"
+bbred="\033[1;101m"
+bbgreen="\033[1;102m"
+bbyellow="\033[1;103m"
+bbblue="\033[1;104m"
+bbmagenta="\033[1;105m"
+bbcyan="\033[1;106m"
+bbwhite="\033[1;107m"
+
+# reset
+
+rset="\033[0m"
