5e202cb306e8fd6ddc0d7d41df9ce93ee344a8e2
lulboi6000 <161829134+lulboi6000@users.noreply.github.com>
2025-01-02 15:48:07 +0600

    Update coldefs.sh

--- coldefs.sh
+++ coldefs.sh
@@ -2,56 +2,56 @@
 
 # forground normal colors
 
-fblack="\033[1;30m"
-fred="\033[1;31m"
-fgreen="\033[1;32m"
-fyellow="\033[1;33m"
-fblue="\033[1;34m"
-fmagenta="\033[1;35m"
-fcyan="\033[1;36m"
-fwhite="\033[1;37m"
+FBLACK="\033[1;30m"
+FRED="\033[1;31m"
+FGREEN="\033[1;32m"
+FYELLOW="\033[1;33m"
+FBLUE="\033[1;34m"
+FMAGENTA="\033[1;35m"
+FCYAN="\033[1;36m"
+FWHITE="\033[1;37m"
 
 # forground bright colors
 
-fbblack="\033[1;90m"
-fbred="\033[1;91m"
-fbgreen="\033[1;92m"
-fbyellow="\033[1;93m"
-fbblue="\033[1;94m"
-fbmagenta="\033[1;95m"
-fbcyan="\033[1;96m"
-fbwhite="\033[1;97m"
+FBBLACK="\033[1;90m"
+FBRED="\033[1;91m"
+FBGREEN="\033[1;92m"
+FBYELLOW="\033[1;93m"
+FBBLUE="\033[1;94m"
+FBMAGENTA="\033[1;95m"
+FBCYAN="\033[1;96m"
+FBWHITE="\033[1;97m"
 
 # background normal colors
 
-bblack="\033[1;40m"
-bred="\033[1;41m"
-bgreen="\033[1;42m"
-byellow="\033[1;43m"
-bblue="\033[1;44m"
-bmagenta="\033[1;45m"
-bcyan="\033[1;46m"
-bwhite="\033[1;47m"
+BBLACK="\033[1;40m"
+BRED="\033[1;41m"
+BGREEN="\033[1;42m"
+BYELLOW="\033[1;43m"
+BBLUE="\033[1;44m"
+BMAGENTA="\033[1;45m"
+BCYAN="\033[1;46m"
+BWHITE="\033[1;47m"
 
 # background bright colors
 
-bbblack="\033[1;100m"
-bbred="\033[1;101m"
-bbgreen="\033[1;102m"
-bbyellow="\033[1;103m"
-bbblue="\033[1;104m"
-bbmagenta="\033[1;105m"
-bbcyan="\033[1;106m"
-bbwhite="\033[1;107m"
+BBBLACK="\033[1;100m"
+BBRED="\033[1;101m"
+BBGREEN="\033[1;102m"
+BBYELLOW="\033[1;103m"
+BBBLUE="\033[1;104m"
+BBMAGENTA="\033[1;105m"
+BBCYAN="\033[1;106m"
+BBWHITE="\033[1;107m"
 
 # foreground rgb color, requires true color support
 
-frgb="\033[38;2;"
+FRGB="\033[38;2;"
 
 # background rgb color, requires true color support
 
-brgb="\033[48;2;"
+BRGB="\033[48;2;"
 
 # reset
 
-reset="\033[0m"
+RESET="\033[0m"
